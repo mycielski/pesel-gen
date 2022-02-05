e85f25977a1e43c45b481631cb456479a005dbba
mycielski <mycielski00@gmail.com>
2022-02-05 14:13:55 +0100

    remove useless fstring

---
+++ examples/zip_password.py
@@ -0,0 +1,15 @@
+import os
+
+
+def main():
+    os.system("zip -P 02070803628 encrypted.zip standard-document.pdf")
+    os.system("../target/release/pesel-gen -g f -b 8-07-1902 > ./wordlist.txt")
+    os.system(
+        "fcrackzip --use-unzip --dictionary --init-password ./wordlist.txt ./encrypted.zip"
+    )
+    os.system("rm ./wordlist.txt")
+    os.system("rm ./encrypted.zip")
+
+
+if __name__ == "__main__":
+    main()
