ac25dd35c46b33d0483f1f4537d17cb1c22047e0
mycielski <mycielski00@gmail.com>
2022-02-05 16:04:33 +0100

    add both genders support

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ use pesel_gen::{date, generation};
 #[derive(Parser, Debug)]
 #[clap()]
 struct Args {
-    /// Either "m" or "f".
-    #[clap(short, long)]
-    gender: char,
-
     /// Birthday in format "dd-mm-yyyy".
     #[clap(short, long)]
     birthday: String,
+
+    /// Use "m" for males, "f" for females and "b" for both.
+    #[clap(short, long)]
+    gender: char,
 }
 
 fn main() {
     let args = Args::parse();
 
     let birthday = date::date_from_arg(args.birthday);
-    generation::generate_pesel_numbers(birthday, args.gender);
+
+    if args.gender == 'm' || args.gender == 'b' {
+        generation::generate_pesel_numbers(birthday, 'm');
+    }
+    if args.gender == 'f' || args.gender == 'b' {
+        generation::generate_pesel_numbers(birthday, 'f');
+    }
 }
