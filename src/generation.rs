dc56f9779285478864e67b7fbcc3fd5f49eabf14
mycielski <mycielski00@gmail.com>
2022-02-07 01:00:24 +0100

    performance increase

--- src/generation.rs
+++ src/generation.rs
@@ -11,9 +11,8 @@ pub fn generate_pesel_numbers(birthday: Date, gender: char) {
 
     let rrmmdd = date::rrmmdd_from_date(birthday);
 
-    for i in 000..=999 {
-        let ppp: String = format!("{:03}", i);
-        for value in (g..10).step_by(2) {
+    for value in (g..10).step_by(2) {
+        for ppp in 000..=999 {
             let last_digit = calculate_checksum(format!("{:06}{:03}{:01}", rrmmdd, ppp, value));
             println!("{}{}{}{}", rrmmdd, ppp, value, last_digit);
         }
